c1455399547088f839bd57b5d7a26e45a7eb4791
Minle Liang <minleliang2002@gmail.com>
2024-05-02 23:28:19 -0400

    3

--- api.py
+++ api.py
@@ -16,20 +16,9 @@ def load_disease_advice():
         return {}
     
 def find_close_matches(input_str, data, cutoff=0.5, n=1):
-    # Convert input string to lowercase
-    input_str_lower = input_str.lower()
-    
-    # Convert keys in data dictionary to lowercase
-    data_lower = {key.lower(): value for key, value in data.items()}
-    
-    keys = list(data_lower.keys())
-    
-    # Find close matches using lowercase input and keys
-    close_matches = get_close_matches(input_str_lower, keys, n=n, cutoff=cutoff)
-    
-    # Retrieve original case matches and their corresponding values
-    matches_info = [(original_case_key, data[original_case_key]) for original_case_key in close_matches]
-    
+    keys = list(data.keys())
+    close_matches = get_close_matches(input_str, keys, n=n, cutoff=cutoff)
+    matches_info = [(match, data[match]) for match in close_matches]
     return matches_info
 
 @app.route('/medicine', methods=['POST'])
@@ -43,7 +32,7 @@ def detect_medicine():
         if matched_info:
             response_data = {
                 "matches": [
-                    {"medicine": match, "Uses": info['Uses'], "Side Effects:": info['Side Effects']}
+                    {"medicine": match, "Uses": info['Uses'], "Side Effects": info['Side Effects']}
                     for match, info in matched_info
                 ]
             }
@@ -55,4 +44,4 @@ def detect_medicine():
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)  
